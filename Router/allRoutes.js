4bd1e4e55f7edd74ea58187695a187d5274f728c
Jessica <abdoulahi.jessica@gmail.com>
2024-09-13 22:15:04 +0200

    Création de la pahe nosanimaux.html, ajout au fichier allRoutes.js

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,6 +3,7 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/lezoo", "Le zoo", "/pages/lezoo.html"),
+    new Route("/nosanimaux", "Nos animaux", "/pages/nosanimaux.html"),
 ];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Zoo Arcadia";
