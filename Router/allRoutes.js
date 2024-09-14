dff9b3e306ec7135a71dfeb332e0b9b170a00104
Jessica <abdoulahi.jessica@gmail.com>
2024-09-15 00:08:40 +0200

    Création d'une page tarifsetservices.html et des routes

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,6 +5,7 @@ export const allRoutes = [
     new Route("/lezoo", "Le zoo", "/pages/lezoo.html"),
     new Route("/nosanimaux", "Nos animaux", "/pages/nosanimaux.html"),
     new Route("/contact", "Contact", "/pages/contact.html"),
+    new Route("/tarifsetservices", "Tarifs et Services", "/pages/tarifsetservices.html"),
 ];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Zoo Arcadia";
