bc0ca30718ca4e9589ad4e97afde01f457d03159
Jessica <abdoulahi.jessica@gmail.com>
2024-09-17 00:57:26 +0200

    Création d'une nouvelle page pour indiquer plus de détails sur l'habitat savane et les animaux

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -6,6 +6,7 @@ export const allRoutes = [
     new Route("/nosanimaux", "Nos animaux", "/pages/nosanimaux.html"),
     new Route("/contact", "Contact", "/pages/contact.html"),
     new Route("/tarifsetservices", "Tarifs et Services", "/pages/tarifsetservices.html"),
+    new Route("/savane-animaux", "La savane et ses abimaux", "/pages/savane-animaux.html"),
 ];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Zoo Arcadia";
