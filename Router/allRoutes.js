d33e51ec455b41cba9809662e00c4a39e369e889
Jessica <abdoulahi.jessica@gmail.com>
2024-09-17 02:19:44 +0200

    Création nouvelle page pour un animal et d'un dossier images pour les animaux

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -7,6 +7,7 @@ export const allRoutes = [
     new Route("/contact", "Contact", "/pages/contact.html"),
     new Route("/tarifsetservices", "Tarifs et Services", "/pages/tarifsetservices.html"),
     new Route("/savane-animaux", "La savane et ses abimaux", "/pages/savane-animaux.html"),
+    new Route("/winnie-antilope", "Winnie l'antilope","/pages/winnie-antilope.html"),
 ];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Zoo Arcadia";
