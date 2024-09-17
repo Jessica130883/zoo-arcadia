6f4e8d354190d4f0d34509d0554a60f8e5518d04
Jessica <abdoulahi.jessica@gmail.com>
2024-09-17 23:16:33 +0200

    Ajout d'une image éléphant, ajout description de l'habitat et des cards des animaux

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -7,7 +7,7 @@ export const allRoutes = [
     new Route("/contact", "Contact", "/pages/contact.html"),
     new Route("/tarifsetservices", "Tarifs et Services", "/pages/tarifsetservices.html"),
     new Route("/savane-animaux", "La savane et ses animaux", "/pages/savane-animaux.html"),
-    new Route("/jungle-animaux", "La jungle et ses animaux", "/pages/jungle-animaux"),
+    new Route("/jungle-animaux", "La jungle et ses animaux", "/pages/jungle-animaux.html"),
 ];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Zoo Arcadia";
